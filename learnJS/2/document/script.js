4441529d368a65d1c0d22b79353051a3e7d4b3e8
Victor <V.Fedorov@bellerage.com>
2022-04-06 13:59:26 +0300

    update

---
+++ learnJS/2/document/script.js
@@ -0,0 +1,8 @@
+const div = document.body.firstElementChild;
+const ul = div.nextElementSibling;
+const li = ul.lastElementChild;
+
+console.log(li);
+console.log(ul.children[0].previousSibling);
+
+
